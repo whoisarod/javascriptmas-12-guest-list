a756858d4cea39411d12928002a9fcb23083f772
Angel Rodriguez <83923623+whoisarod@users.noreply.github.com>
2021-12-11 19:18:30 -0800

    updated index.js and index.html

--- index.js
+++ index.js
@@ -3,6 +3,18 @@ const btn = document.getElementById("btn")
 let guestList = document.getElementById("guest-list")
 let guests = ["Partner", "Nice Relative 1", "Nice Relative 2", "Evil Relative", "Lonely Neighbour"]
 
+function updatedList() {
+    for (let i = 0; i < guests.length; i++) {
+        guestList.innerHTML += "<li>" + guests[i] + "</li>";
+    }
+}
+updatedList()
+
+btn.addEventListener("click", function() {
+    guests.push(input.value)
+    guestList.innerHTML += "<li>" + input.value + "</li>"
+})
+
 // Tasks:
 // - Write the JS to render the Christmas day guest list in the guest-list element.
 // - Add the functionality to add new guests.
